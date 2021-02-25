9be669e17edbc2fec9501919b6eff38897bbb5a6
ClaudiuS20 <clautu.sandu@gmail.com>
2021-02-25 18:57:31 +0200

    git commit -m "al doilea commit"

--- Seminar_1/src/pachet/Program.java
+++ Seminar_1/src/pachet/Program.java
@@ -1,7 +1,19 @@
 package pachet;
 
-public class Program {
-	public static void main(String[] args) {
+import clase.Girafa;
+import clase.Zoo;
 
+public class Program {
+	
+	public static void main(String[] args) {
+		Zoo zoo = new Zoo();
+		
+		Girafa g1 = new Girafa("g1");
+		Girafa g2 = new Girafa("g2");
+		
+		zoo.adauga(g1);
+		zoo.adauga(g2);
+		
+		zoo.FeedAnimals();
     }
 }
