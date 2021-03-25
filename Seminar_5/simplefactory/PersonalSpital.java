3a7a7b8407adb81b958f4f77839034e9e3f08fbc
ClaudiuS20 <61415624+ClaudiuS20@users.noreply.github.com>
2021-03-25 19:26:37 +0200

    Simple Factory
    Problema A, cerinta 2

---
+++ Seminar_5/simplefactory/PersonalSpital.java
@@ -0,0 +1,15 @@
+package ro.ase.cts.simplefactory;
+
+public abstract class PersonalSpital {
+	private String nume;
+
+	public PersonalSpital(String nume) {
+		super();
+		this.nume = nume;
+	}
+
+	public String getNume() {
+		return nume;
+	}
+	
+}
---
+++ Seminar_5/simplefactory/TipPersonal.java
@@ -0,0 +1,7 @@
+package ro.ase.cts.simplefactory;
+
+public enum TipPersonal {
+	Brancardier,
+	Asistent,
+	Medic
+}
