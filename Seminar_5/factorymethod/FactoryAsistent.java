9f6b2804d8a467348aabb323213eac468b8935d4
ClaudiuS20 <61415624+ClaudiuS20@users.noreply.github.com>
2021-03-25 19:36:23 +0200

    Delete Seminar_5/factorymethod directory

--- Seminar_5/factorymethod/FactoryAsistent.java
+++
@@ -1,9 +0,0 @@
-package ro.ase.cts.factorymethod;
-
-public class FactoryAsistent implements IFactory {
-	
-	@Override
-	public PersonalSpital getPersonal(String nume) {
-		return new Asistent(nume);
-	}
-}
--- Seminar_5/factorymethod/IFactory.java
+++
@@ -1,7 +0,0 @@
-package ro.ase.cts.factorymethod;
-
-public interface IFactory {
-
-	public PersonalSpital getPersonal(String nume);
-	
-}
